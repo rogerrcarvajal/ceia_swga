a1e16500e8c2810a721607b7443fd00063921419
rogerrcarvajal <roger.carvajal@yahoo.com.ve>
2025-07-09 00:37:15 -0400

    Refactor estilos del menú desplegable: ajustar el color de fondo, agregar sombra y establecer un ancho máximo.

--- src/templates/navbar.php
+++ src/templates/navbar.php
@@ -86,10 +86,13 @@
         position: absolute;
         min-width: 160px;
         z-index: 1;
-        background-color: rgba(0, 0, 0, 0.5);
+        background-color: rgba(0, 0, 0, 0.3);
         backdrop-filter:blur(10px);
         box-shadow: 0px 0px 10px rgba(227,228,237,0.37);
         border:2px solid rgba(255,255,255,0.18);
+        filter: drop-shadow(0.25rem 0.25rem 0.5rem rgba(0, 0, 0, 0.3));
+        width: 100%;
+        max-width: 14rem;
     }
 
     .dropdown-content a {
